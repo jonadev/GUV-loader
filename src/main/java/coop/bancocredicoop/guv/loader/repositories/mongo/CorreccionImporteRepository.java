336fa62cac8ac3d3891e382731b15b3ffb0a15ee
jvillarroel <jvillarroel@credicoop.coop>
2019-02-07 11:32:43 -0300

    WIP mongo

--- src/main/java/coop/bancocredicoop/guv/loader/repositories/mongo/CorreccionImporteRepository.java
+++ src/main/java/coop/bancocredicoop/guv/loader/repositories/mongo/CorreccionImporteRepository.java
@@ -1,6 +1,7 @@
 package coop.bancocredicoop.guv.loader.repositories.mongo;
 
 import coop.bancocredicoop.guv.loader.models.mongo.CorreccionCheque;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 public interface CorreccionImporteRepository extends ReactiveMongoRepository<CorreccionCheque, Long> {
 
